b066d573647a3b43d9eb060362001fa9492caff9
zcota <zcota@email.arizona.edu>
2017-02-25 12:46:14 -0700

    Added new comment

--- first_script.sh
+++ first_script.sh
@@ -1,3 +1,11 @@
+
+if [ "$1" == "-h" ]; then
+    echo "Usage: sh $0 input_file output_file"
+    exit
+
+fi
+
+
 #This script is used to count the number of characters in a file
 head -5 $1 | sort | wc -c >  $2
 
